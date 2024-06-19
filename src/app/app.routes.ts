d573edbe849070d97adf6bcae53e3faae7895d97
Juliana Cardona <julicardonco@hotmail.com>
2024-06-18 23:26:12 -0500

    Creación de página login y registro con rutas y modificación de colores globales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { ProductsListComponent } from './pages/products-list/products-list.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'detail', component: ProductDetailComponent},
-    { path: 'productslist', component: ProductsListComponent}
+    { path: 'productslist', component: ProductsListComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent}
 ];
